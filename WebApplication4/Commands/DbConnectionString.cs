e77660642264ee6b0153259419f2a22dbbe0d48b
Edsel Clarin <edsel.clarin@yahoo.com>
2022-08-31 22:53:10 -0700

    Moved database out of App_Data.

--- WebApplication4/Commands/DbConnectionString.cs
+++ WebApplication4/Commands/DbConnectionString.cs
@@ -3,6 +3,6 @@
     public static class DbConnectionString
     {
         public static string ConnectionString =>
-            @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Edsel\Documents\Projects\WebApplication4\WebApplication4\App_Data\Database1.mdf;Integrated Security=True";
+            @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Edsel\Documents\Projects\WebApplication4\WebApplication4\Database2.mdf;Integrated Security=True";
     }
 }
